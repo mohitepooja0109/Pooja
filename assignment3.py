1f841daaa344c2bf9c5bcf6d46760b8e4b10cc12
mohitepooja0109 <137404334+mohitepooja0109@users.noreply.github.com>
2023-06-27 12:14:48 +0530

    Add files via upload

--- assignment3.py
+++ assignment3.py
@@ -1,24 +1,28 @@
-# Program to display the Fibonacci sequence up to n-th term
-nterms = int(input("How many terms? "))
+# list of numbers
+list1 = [12,-7,5,64,-14]
+num = 0
+ 
+# using while loop    
+while(num < len(list1)):
+     
+    # checking condition
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+     
+    # increment num
+    num += 1
 
-# first two terms
-n1, n2 = 0, 1
-count = 0
-
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Please enter a positive integer")
-# if there is only one term, return n1
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
-# generate fibonacci sequence
-else:
-   print("Fibonacci sequence:")
-   while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+    
+### list of numbers
+list2 = [12,14,-95,3]
+num = 0
+ 
+# using while loop    
+while(num < len(list2)):
+     
+    # checking condition
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+     
+    # increment num
+    num += 1
