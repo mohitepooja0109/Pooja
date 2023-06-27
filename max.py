97e18fd5033fd531ca4fd38e1eed097aeac8135f
mohitepooja0109 <137404334+mohitepooja0109@users.noreply.github.com>
2023-06-27 12:01:24 +0530

    Add files via upload

--- max.py
+++ max.py
@@ -1,21 +1,28 @@
-##Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. Use dictionaries.
+# list of numbers
+list1 = [12,-7,5,64,-14]
+num = 0
+ 
+# using while loop    
+while(num < len(list1)):
+     
+    # checking condition
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+     
+    # increment num
+    num += 1
 
-def most_frequent(string):
-    # Create an empty dictionary to store the letter frequencies
-    freq_dict = {}
-
-    # Count the frequency of each letter in the string
-    for letter in string:
-        freq_dict[letter] = freq_dict.get(letter, 0) + 1
-
-    # Sort the dictionary by values in descending order
-    sorted_dict = dict(sorted(freq_dict.items(), key=lambda x: x[1], reverse=True))
-
-    # Print the letters in decreasing order of frequency
-    for letter, frequency in sorted_dict.items():
-        print(letter, frequency)
-
-
-# Example usage
-input_string = input("Enter a string: ")
-most_frequent(input_string)
+    
+### list of numbers
+list1 = [12,14,-95,3]
+num = 0
+ 
+# using while loop    
+while(num < len(list1)):
+     
+    # checking condition
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+     
+    # increment num
+    num += 1
