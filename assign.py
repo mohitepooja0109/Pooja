687845e0cdf040d176e1f5f2073ee27089cdc548
mohitepooja0109 <137404334+mohitepooja0109@users.noreply.github.com>
2023-06-27 12:30:59 +0530

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,28 @@
+# list of numbers
+list1 = [12,-7,5,64,-14]
+num = 0
+ 
+# using while loop    
+while(num < len(list1)):
+     
+    # checking condition
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+     
+    # increment num
+    num += 1
+
+    
+### list of numbers
+list2 = [12,14,-95,3]
+num = 0
+ 
+# using while loop    
+while(num < len(list2)):
+     
+    # checking condition
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+     
+    # increment num
+    num += 1
